c32d772ccd9c4ae0fa13632774c133345035b432
meysam255 <meysam_imanifar88@yahoo.com>
2021-06-10 22:38:46 +0200

    init

---
+++ TopLearn.Core/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TopLearn.DataLayer.Entities.User;
+
+namespace TopLearn.Core.Services.Interfaces
+{
+   public interface IUserService
+   {
+       bool IsExistUserName(string userName);
+       bool IsExistEmail(string email);
+       int AddUser(User user);
+   }
+}
---
+++ TopLearn.Web/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using TopLearn.Core.Convertors;
+using TopLearn.Core.DTOs;
+using TopLearn.Core.Generator;
+using TopLearn.Core.Security;
+using TopLearn.Core.Services.Interfaces;
+using TopLearn.DataLayer.Entities.User;
+
+namespace TopLearn.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            
+            if (_userService.IsExistUserName(register.UserName))
+            {
+                ModelState.AddModelError("UserName","نام کاربری معتبر نمی باشد");
+                return View(register);
+            }
+
+            if (_userService.IsExistEmail(FixedText.FixEmail(register.Email)))
+            {
+                ModelState.AddModelError("Email", "ایمیل معتبر نمی باشد");
+                return View(register);
+            }
+
+
+            DataLayer.Entities.User.User user=new User()
+            {
+                ActiveCode = NameGenerator.GenerateUniqCode(),
+                Email = FixedText.FixEmail(register.Email),
+                IsActive = false,
+                Password = PasswordHelper.EncodePasswordMd5(register.Password),
+                RegisterDate = DateTime.Now,
+                UserAvatar = "Defult.jpg",
+                UserName = register.UserName
+            };
+            _userService.AddUser(user);
+
+            return View("SuccessRegister",user);
+        }
+    }
+}
